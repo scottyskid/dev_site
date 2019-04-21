208e1ed22608298d12544e8455ff31beee759ed2
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-04-22 09:14:39 +1000

    first migration

--- dev_site/settings.py
+++ dev_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'initiative_tracker.apps.InitiativeTrackerConfig'
 ]
 
 MIDDLEWARE = [
--- initiative_tracker/models.py
+++ initiative_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Encounter(models.Model):
+    encounter_name = models.CharField(max_length=200)
+
+
+class Creature(models.Model):
+    encounter = models.ForeignKey(Encounter, on_delete=models.CASCADE)
+    monster_name = models.CharField(max_length=200)
+    initiative_value = models.IntegerField()
